3f7ec6a831c2e15223da43828bcd6484d186cecd
Andrey Sidorov <andrey.sidorov@gmail.com>
2023-02-23 19:07:40 +1100

    uptime

--- server.mjs
+++ server.mjs
@@ -1,6 +1,8 @@
 import { createServer } from 'http';
 
+const startTime = new Date();
+
 createServer((req, res) => {
-  res.write('Hello World!');
+  res.write(`Hello World! ${startTime} ${new Date()} ${process.env.PORT}}`);
   res.end();
 }).listen(process.env.PORT);
